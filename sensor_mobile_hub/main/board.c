147afc7d35b57b6c5ea45836d79d593f1188ef3b
Marcelo Paulon <marcelo.paulon@zwift.com>
2021-05-20 02:02:23 -0300

    fix mobile-hub - should not be a provisioner

--- sensor_mobile_hub/main/board.c
+++ sensor_mobile_hub/main/board.c
@@ -12,7 +12,11 @@
 #include "iot_button.h"
 #include "esp_ble_mesh_sensor_model_api.h"
 
+#include "driver/gpio.h"
+
 #include <time.h>
+
+#include "board.h"
 
 #define TAG "BOARD"
 
@@ -50,7 +54,43 @@ static void board_button_init(void)
     }
 }
 
+struct _led_state led_state[3] = {
+    { LED_OFF, LED_OFF, LED_R, "red"   },
+    { LED_OFF, LED_OFF, LED_G, "green" },
+    { LED_OFF, LED_OFF, LED_B, "blue"  },
+};
+
+void board_led_operation(uint8_t pin, uint8_t onoff)
+{
+    for (int i = 0; i < 3; i++) {
+        if (led_state[i].pin != pin) {
+            continue;
+        }
+        if (onoff == led_state[i].previous) {
+            ESP_LOGW(TAG, "led %s is already %s",
+                     led_state[i].name, (onoff ? "on" : "off"));
+            return;
+        }
+        gpio_set_level(pin, onoff);
+        led_state[i].previous = onoff;
+        return;
+    }
+
+    ESP_LOGE(TAG, "LED is not found!");
+}
+
+static void board_led_init(void)
+{
+    for (int i = 0; i < 3; i++) {
+        gpio_reset_pin(led_state[i].pin);
+        gpio_set_direction(led_state[i].pin, GPIO_MODE_OUTPUT);
+        gpio_set_level(led_state[i].pin, LED_OFF);
+        led_state[i].previous = LED_OFF;
+    }
+}
+
 void board_init(void)
 {
     board_button_init();
+    board_led_init();
 }
