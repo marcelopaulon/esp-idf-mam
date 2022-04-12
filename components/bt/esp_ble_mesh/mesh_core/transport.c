bc9e3f981696547137d4e39aa038b8c67f219230
Marcelo Paulon <3494515+marcelopaulon@users.noreply.github.com>
2022-04-12 18:22:07 -0300

    Increase buffer size; undo WARN change

--- components/bt/esp_ble_mesh/mesh_core/transport.c
+++ components/bt/esp_ble_mesh/mesh_core/transport.c
@@ -1083,8 +1083,8 @@ static int trans_unseg(struct net_buf_simple *buf, struct bt_mesh_net_rx *rx,
         return -EINVAL;
     }
 
-    if (bt_mesh_rpl_check(rx, NULL)) { // TODO: Changed - fixme - from WARN TO DBG
-        BT_DBG("Replay: src 0x%04x dst 0x%04x seq 0x%06x",
+    if (bt_mesh_rpl_check(rx, NULL)) {
+        BT_WARN("Replay: src 0x%04x dst 0x%04x seq 0x%06x",
                 rx->ctx.addr, rx->ctx.recv_dst, rx->seq);
         return -EINVAL;
     }
@@ -1511,8 +1511,8 @@ static int trans_seg(struct net_buf_simple *buf, struct bt_mesh_net_rx *net_rx,
         return -EINVAL;
     }
 
-    if (bt_mesh_rpl_check(net_rx, &rpl)) { // TODO: Changed - fixme - from WARN TO DBG
-        BT_DBG("Replay: src 0x%04x dst 0x%04x seq 0x%06x",
+    if (bt_mesh_rpl_check(net_rx, &rpl)) {
+        BT_WARN("Replay: src 0x%04x dst 0x%04x seq 0x%06x",
                 net_rx->ctx.addr, net_rx->ctx.recv_dst, net_rx->seq);
         return -EINVAL;
     }
